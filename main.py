d95032017d2cef8529ca85c6533f28cfa082948c
akshatsrf <56485798+akshatsrf@users.noreply.github.com>
2022-03-29 12:13:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Number(BaseModel):
+    n: int
+
 @app.post("/")
-def ret_square1(num: int):
-	return num*num
+def ret_square1(num: Number):
+	N = num.n
+	return N*N
 	
 @app.get("/{num}")
 def ret_square2(num):
